d1f7615c6f9fc36671f8842336f6328b73025068
Yash Raval <yashraval05@gmail.com>
2021-09-17 13:29:15 -0400

    Update Find the Union and Intersection of the two sorted arrays..cpp

--- Array/Find the Union and Intersection of the two sorted arrays..cpp
+++ Array/Find the Union and Intersection of the two sorted arrays..cpp
@@ -1,1 +1,16 @@
-yash
+class Solution{
+    public:
+    //Function to return the count of number of elements in union of two arrays.
+    int doUnion(int a[], int n, int b[], int m)  {
+        //code here
+        unordered_set<int> CountDistinct;
+        int i;
+        for (i=0;i<n;i++){
+            CountDistinct.insert(a[i]);
+        }
+        for (i=0;i<m;i++){
+            CountDistinct.insert(b[i]);
+        }
+        return CountDistinct.size();
+    }
+};
