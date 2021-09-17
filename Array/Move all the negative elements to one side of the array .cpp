6ae12c7a52852d12317413e46a583c67dfd22878
Yash Raval <yashraval05@gmail.com>
2021-09-17 13:28:33 -0400

    Update Move all the negative elements to one side of the array .cpp

--- Array/Move all the negative elements to one side of the array .cpp
+++ Array/Move all the negative elements to one side of the array .cpp
@@ -1,1 +1,25 @@
-yash
+void shiftall(int arr[], int left,
+              int right)
+{
+  while (left<=right)
+  {
+    if (arr[left] < 0 && arr[right] < 0) //both neg
+      left+=1;
+     
+    
+    else if (arr[left]>0 && arr[right]<0) //left posi right neg
+    {
+      int temp=arr[left];
+      arr[left]=arr[right];
+      arr[right]=temp;
+      left+=1;
+      right-=1;
+    }
+    else if (arr[left]>0 && arr[right] >0)  //both posi
+      right-=1; 
+    else{
+      left += 1;
+      right -= 1;
+    }
+  }
+}
