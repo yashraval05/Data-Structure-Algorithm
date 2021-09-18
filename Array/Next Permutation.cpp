93978c2a05d25a95a0b66bc97cd9c5808725d4bb
Yash Raval <yashraval05@gmail.com>
2021-09-18 12:05:43 -0400

    Create Next Permutation.cpp

---
+++ Array/Next Permutation.cpp
@@ -0,0 +1,25 @@
+//link to problem https://leetcode.com/problems/next-permutation/
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& a) {
+        int n=a.size(),i,j;
+        for (i=n-2;i>=0;i--){
+            if (a[i]<a[i+1])
+                break;
+        }
+        if(i<0){
+            reverse(a.begin(),a.end());
+        }
+        else{
+        for (j=n-1;j>i;j--){
+            if(a[j]>a[i]){
+                break;
+            }
+        }
+            swap(a[j],a[i]);
+            reverse(a.begin()+i+1,a.end());
+        }
+        
+    }
+};
