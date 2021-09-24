ee2c8a469a1e00b46ff0e1f69dd7029feef693e4
Yash Raval <yashraval05@gmail.com>
2021-09-24 22:36:42 +0530

    Create Plus One.cpp

---
+++ Array/Plus One.cpp
@@ -0,0 +1,20 @@
+// link to problem https://leetcode.com/problems/plus-one/
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+    
+        int i = digits.size()-1;
+        while (i>=0){
+            if (digits[i]==9)
+                digits[i]=0;
+            else{ 
+                digits[i] += 1;
+                return digits;
+            }
+            i--;
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
