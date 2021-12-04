0ee20f866b684136001857d891c4c0f82f7e375e
Yash Raval <yashraval05@gmail.com>
2021-12-04 15:22:56 +0530

    Create Palindrome String.cpp

---
+++ String/Palindrome String.cpp
@@ -0,0 +1,17 @@
+//link to problem https://practice.geeksforgeeks.org/problems/palindrome-string0817/1
+
+	int isPalindrome(string S)
+{
+    // Your code goes here
+    int start=0;
+    int end=S.length()-1;
+    while(start<end){
+        if(S[start]!=S[end])
+            return 0;
+        else{
+            start++;
+            end--;
+        }
+    }
+    return 1;
+}
