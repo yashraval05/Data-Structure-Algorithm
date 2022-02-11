5820d648058a5ddbd0eca5d0794b29823cff4ce1
Yash Raval <yashraval05@gmail.com>
2022-02-11 22:08:03 +0530

    Create Majority Element 169.cpp

---
+++ Array/Majority Element 169.cpp
@@ -0,0 +1,24 @@
+//link to problem https://leetcode.com/problems/majority-element/
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int majority=nums[0];
+        int count = 1;
+        for (int i=1;i<n;i++){
+            if(nums[i]==majority)
+                count++;
+            else{
+                count--;
+                if(count==0){
+                    majority=nums[i];
+                    count=1;
+                }
+                
+            }
+        }
+        return majority;
+    }
+};
